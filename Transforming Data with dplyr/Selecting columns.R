f760cd0607391476a4d646af809ff431caaefce6
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-14 11:15:16 -0600

    Create Selecting columns.R

---
+++ Transforming Data with dplyr/Selecting columns.R
@@ -0,0 +1,14 @@
+# Select the following four columns from the counties variable:
+
+#    state
+#    county
+#    population
+#    poverty
+
+# You don't need to save the result to a variable.
+ 
+#    Select the columns listed from the counties variable.
+
+# Select the columns 
+counties %>%
+  select(state, county, population, poverty)
